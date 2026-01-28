89858fb0fa83b786c33193d4500a59da09f86a5e
jabberwocky238 <7176656@qq.com>
2026-01-27 21:12:11 -0500

    测试

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { Combinator } from 'combinator-sdk'
+import { Hono } from 'hono'
+import * as dotenv from 'dotenv'
+
+dotenv.config()
+const app = new Hono()
+
+const baseURL = () => {
+  if (process.env.VITE_MODE === 'development') {
+    return 'http://localhost:8899/'
+  } else {
+    return 'http://combinator.app238.cn/'
+  }
+}
+
+const combinator = new Combinator({
+  baseURL: baseURL(),
+})
+
+const rdb = combinator.rdb('0')
+
+app.get('/', (c) => {
+  return c.html('<h1>Hello Combinator RDB!</h1>')
+})
+
+app.get('/init', async (c) => {
+  await rdb.exec("CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT);")
+  return c.text('Table initialized')
+})
+
+app.get('/incre', async (c) => {
+  const timestamp = Date.now()
+  await rdb.exec(`INSERT INTO test (name) VALUES ('Timestamp: ${timestamp}');`)
+  return c.text('Inserted timestamp' + timestamp)
+})
+
+app.get('/data', async (c) => {
+  const result = await rdb.query('SELECT * FROM test;')
+  return c.json(result)
+})
+
+app.get('/mode', (c) => {
+  return c.json({ message: process.env.VITE_MODE })
+})
+
+export default app
