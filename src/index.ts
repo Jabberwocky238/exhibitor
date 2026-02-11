ad9036e25852096a73b90c02a82ed1ee323ce862
jabberwocky238 <7176656@qq.com>
2026-02-11 10:06:33 -0500

    ?

--- src/index.ts
+++ src/index.ts
@@ -15,9 +15,7 @@ const baseURL = () => {
   }
 }
 
-const combinator = new Combinator({
-  baseURL: baseURL(),
-})
+const combinator = new Combinator()
 
 const rdb = combinator.rdb('4f710875efe061a7')
 
